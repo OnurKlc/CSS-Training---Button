1aff101aa83ab4c8b23604da72075df6194c6bf6
OnurKlc <onurkilic5@gmail.com>
2020-08-06 01:04:07 +0300

    cleaned alghoritm problems

--- 1/index.js
+++
@@ -1,8 +0,0 @@
-const sampleArray = [1, 2, 3, 4];
-main(sampleArray)
-
-function main(arr) {
-  let result = arr.reduce((a, b) => a + b, 0);
-  console.log("The sum of numbers is", result);
-  return result;
-};
